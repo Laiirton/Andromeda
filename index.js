58e287962d40c2bf3939630a10d7ef5aa6ac05e3
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-02 17:43:45 -0300

    remove auth session

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 import pkg from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 import { NSFW } from "nsfwhub";
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
-//library
 const { Client, LocalAuth, MessageMedia } = pkg;
 const nsfw = new NSFW();
+
+
+// AI GOOGLE API
+const apiKey = "AIzaSyCem06DOhmjJxz9qireL64r4Nt8L3lyVk0";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function getGeminiResponse(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ],
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
 
 
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    executablePath: "/usr/bin/google-chrome",
     headless: true,
 
   }
@@ -25,6 +69,17 @@ client.on("qr", (qr) => {
 });
 
 client.on("message", async (message) => {
+
+  if (message.body.startsWith("coiso")) 
+    {
+    const prompt = message.body.replace("coiso", "").trim();
+    const response = await getGeminiResponse(prompt);
+    console.log(response);
+    }
+
+
+
+
   if (message.body.startsWith("!")) {
     const [command, ...queryWords] = message.body
       .toLowerCase()
