338325228ee347f87ba4f531bbe81a089d63591f
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-02 17:30:54 -0300

    update menu command

--- index.js
+++ index.js
@@ -114,10 +114,6 @@ client.on("message", async (message) => {
           
           *!fig* - Cria uma figurinha a partir de uma imagem. Responda a uma imagem com este comando.
           *!img* - Envia a imagem original de uma figurinha. Responda a uma figurinha com este comando.
-          *!pussy* - Envia uma imagem aleatória de uma vagina.
-          *!ass* - Envia uma imagem aleatória de um bumbum.
-          *!dick* - Envia uma imagem aleatória de um pênis.
-          *!menu* - Mostra este menu de ajuda.
         `);
         break;
 
