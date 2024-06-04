3119e3aef651e808adf7adf17888a18c85d0d7f5
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 02:17:05 -0300

    translato to eng

--- src/index.js
+++ src/index.js
@@ -4,40 +4,40 @@ import { processMessage } from "./controllers/messageController.js";
 const client = initWhatsappClient();
 
 client.on("ready", () => {
-  console.log("Cliente está pronto!");
+  console.log("Client is ready!");
 });
 
 client.on("message", async (message) => {
   try {
     await processMessage(client, message);
   } catch (error) {
-    console.error(`Erro ao processar a mensagem: ${error}`);
+    console.error(`Error processing message: ${error}`);
   }
 });
 
 client.on("auth_failure", () => {
-  console.error("Falha de autenticação. Verifique suas credenciais.");
+  console.error("Authentication failure. Check your credentials.");
 });
 
 client.on("disconnected", (reason) => {
-  console.log(`Cliente foi desconectado, motivo: ${reason}. Reconectando...`);
+  console.log(`Client was disconnected, reason: ${reason}. Reconnecting...`);
   client.initialize();
 });
 
 client.on("change_state", (state) => {
-  console.log(`O estado do cliente mudou para: ${state}`);
+  console.log(`Client state changed to: ${state}`);
 });
 
 client.on("qr", (qr) => {
-  console.log(`QR recebido, escaneie para conectar: ${qr}`);
+  console.log(`QR received, scan to connect: ${qr}`);
 });
 
 client.on("message_ack", (message, ack) => {
-  console.log(`Mensagem ${message.id} foi ${ack}`);
+  console.log(`Message ${message.id} was ${ack}`);
 });
 
 client.on("error", (error) => {
-  console.error(`Erro no cliente: ${error}`);
+  console.error(`Client error: ${error}`);
 });
 
 client.initialize();
