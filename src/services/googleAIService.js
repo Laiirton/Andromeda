974837e121562927d7655739644473475c241607
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 00:07:52 -0300

    add googleaiservice

---
+++ src/services/googleAIService.js
@@ -0,0 +1,44 @@
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+const apiKey = "AIzaSyCem06DOhmjJxz9qireL64r4Nt8L3lyVk0";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+export async function getGeminiResponse(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ],
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
