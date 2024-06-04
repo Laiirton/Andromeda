c2899371ed7d4b9f8d01244d844ac25d7d7955a4
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 00:07:20 -0300

    add media service

---
+++ src/services/mediaService.js
@@ -0,0 +1,49 @@
+import { NSFW } from "nsfwhub";
+import { MessageMedia } from "whatsapp-web.js";
+
+const nsfw = new NSFW();
+
+export async function sendSticker(client, message, senderName) {
+  try {
+    let mediaMessage = message;
+    if (message.hasQuotedMsg) {
+      mediaMessage = await message.getQuotedMessage();
+    }
+    if (mediaMessage.hasMedia) {
+      const media = await mediaMessage.downloadMedia();
+      await client.sendMessage(message.from, media, {
+        sendMediaAsSticker: true,
+        stickerAuthor: "Anjinho Bot ",
+        stickerName: `Create by ${senderName}`,
+      });
+    }
+    console.log(`Sticker sent to ${senderName}`);
+  } catch (error) {
+    console.error(error);
+    message.reply("Failed to fetch image.");
+  }
+}
+
+export async function sendImage(client, message) {
+  let figbase = message;
+  if (message.hasQuotedMsg) {
+    figbase = await message.getQuotedMessage();
+  }
+  if (figbase.hasMedia) {
+    const media = await figbase.downloadMedia();
+    await client.sendMessage(message.from, media);
+  }
+}
+
+export async function sendNSFWImage(client, message, senderName, category) {
+  try {
+    const nsfw_image = await nsfw.fetch(category);
+    const image_url = nsfw_image.image.url;
+    const media = await MessageMedia.fromUrl(image_url);
+    await client.sendMessage(message.from, media);
+    console.log(`${category} image sent to ${senderName}`);
+  } catch (error) {
+    console.error(error);
+    message.reply("Failed to fetch image.");
+  }
+}
