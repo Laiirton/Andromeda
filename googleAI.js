47ec08e8bbc98f3f35cc41a562629dc903958e1f
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 01:01:29 -0300

    refactor: Update Google AI API key, add retry logic in googleAI.js, and improve error handling

--- googleAI.js
+++ googleAI.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
-const apiKey = "AIzaSyCem06DOhmjJxz9qireL64r4Nt8L3lyVk0"
+const apiKey = "AIzaSyCem06DOhmjJxz9qireL64r4Nt8L3lyVk0";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
@@ -15,33 +15,49 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-const MAX_RETRIES = 5;
-const RETRY_DELAY = 2000;
+const MAX_RETRIES = 5; // número máximo de tentativas
+const RETRY_DELAY = 2000; // atraso em milissegundos entre as tentativas
 
 export async function getGeminiResponse(prompt) {
-  const chatSession = model.startChat({
-    generationConfig,
-    safetySettings: [
-      {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
-      },
-    ],
-    history: [],
-  });
+  let retries = 0;
+  let result;
 
-  const result = await chatSession.sendMessage(prompt);
+  while (retries < MAX_RETRIES) {
+    try {
+      const chatSession = model.startChat({
+        generationConfig,
+        safetySettings: [
+          {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+        ],
+        history: [],
+      });
+
+      result = await chatSession.sendMessage(prompt);
+      break; // se a solicitação for bem-sucedida, saia do loop
+    } catch (error) {
+      retries++;
+      if (retries === MAX_RETRIES) {
+        throw error; // se atingir o número máximo de tentativas, lance o erro
+      }
+      console.error(`Error: ${error.message}, retrying in ${RETRY_DELAY / 1000} seconds...`);
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY)); // aguarde antes de tentar novamente
+    }
+  }
+
   return result.response.text();
 }
