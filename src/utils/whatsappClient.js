8db52b4e24a3aa1f467d3382e115a6efd0369553
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 00:06:08 -0300

    add whatsapp client

---
+++ src/utils/whatsappClient.js
@@ -0,0 +1,19 @@
+import pkg from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const { Client, LocalAuth } = pkg;
+
+export function initWhatsappClient() {
+  const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+      headless: true,
+    },
+  });
+
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
+
+  return client;
+}
