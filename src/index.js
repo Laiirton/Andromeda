4758db2202fdadf089dbca5269b5d3884c372a49
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-04 02:15:48 -0300

    refactor: Update WhatsApp client event handling and error logging

--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
 import { initWhatsappClient } from "./utils/whatsappClient.js";
 import { processMessage } from "./controllers/messageController.js";
 
-// Inicialize o cliente do WhatsApp
 const client = initWhatsappClient();
 
-// Quando o cliente estiver pronto
 client.on("ready", () => {
   console.log("Cliente está pronto!");
 });
 
-// Quando uma mensagem é recebida
 client.on("message", async (message) => {
   try {
-    // Processa a mensagem
     await processMessage(client, message);
   } catch (error) {
-    // Loga qualquer erro que ocorra durante o processamento da mensagem
     console.error(`Erro ao processar a mensagem: ${error}`);
   }
 });
 
-// Quando o cliente é desconectado
+client.on("auth_failure", () => {
+  console.error("Falha de autenticação. Verifique suas credenciais.");
+});
+
 client.on("disconnected", (reason) => {
   console.log(`Cliente foi desconectado, motivo: ${reason}. Reconectando...`);
   client.initialize();
 });
 
-// Quando ocorre um erro
+client.on("change_state", (state) => {
+  console.log(`O estado do cliente mudou para: ${state}`);
+});
+
+client.on("qr", (qr) => {
+  console.log(`QR recebido, escaneie para conectar: ${qr}`);
+});
+
+client.on("message_ack", (message, ack) => {
+  console.log(`Mensagem ${message.id} foi ${ack}`);
+});
+
 client.on("error", (error) => {
   console.error(`Erro no cliente: ${error}`);
 });
 
-// Inicializa o cliente
 client.initialize();
