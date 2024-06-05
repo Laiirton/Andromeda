a37bb67777e395f9457af9b5c6a133981ac00e38
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-05 01:57:22 -0300

    refactor: Add new menu options to lang.js

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -4,6 +4,13 @@ const menu = `
 '*!pussy*': 'Envia uma imagem aleatória de uma vagina.',
 '*!ass*': 'Envia uma imagem aleatória de um bumbum.',
 '*!dick*': 'Envia uma imagem aleatória de um pênis.'
+'*!gay*': 'Envia uma imagem aleatória da categoria gay.'
+'*!futa*': 'Envia uma imagem aleatória da categoria futanari.'
+'*!hentai*': 'Envia uma imagem aleatória da categoria hentai.'
+'*!boobs*': 'Envia uma imagem aleatória de um peito feminino.'
+
+
+
 `;
 
 export default menu; 
