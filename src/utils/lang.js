b9c57f41bbb0ad09119e25bf1d1022830f1c42d2
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-25 17:35:49 -0300

    update lang

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -1,5 +1,6 @@
 const menu = `
 Você pode perguntar coisas ao coiso, tipo o chat gpt(Essa versão de modelo é politicamente correta). Basta digitar "coiso" e a pergunta. Exemplo: "coiso qual é a cor do céu?".
+Obs para pessoas burras: Se você tentar pedir algo que seja considerado contra os direitos humanos ele não vai responder.
 
 *!fig*: 'Cria uma figurinha a partir de uma imagem ou vídeo de até 5 segundos.'
 *!transcribe*: 'Use esse comando em um audio para transcrever o audio em texto.'
