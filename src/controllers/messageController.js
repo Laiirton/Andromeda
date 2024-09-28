e410be2f6a3b75f40a282d37cfacfe4920e6b219
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-28 03:20:04 -0300

    feat: pokemon catcher

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -6,22 +6,39 @@ import {
 } from "../services/mediaService.js";
 import { getGeminiResponse } from "../services/googleAIService.js";
 import { menu, menuNSFW } from "../utils/lang.js";
-import { deleteMessage, messageLog, printGroupList } from "../utils/chatTools.js";
+import {
+  deleteMessage,
+  messageLog,
+  printGroupList,
+} from "../utils/chatTools.js";
 import { ollamaGenerate } from "../services/ollama.js";
 import { whisperTranscription } from "../services/whisper.js";
+import {
+  getRandomPokemonNameAndImage,
+  getUserPokemon,
+} from "../services/pokemon.js";
+import pkg from "whatsapp-web.js";
+const { MessageMedia } = pkg;
 
 const EMPTY_PROMPT_ERROR = "O prompt não pode estar vazio.";
 const PROMPT_REPLY = "Oi, você precisa me dizer o que deseja.";
 
-async function handleGenerativeAI(message, senderName, keyword, generateResponse) {
+async function handleGenerativeAI(
+  message,
+  senderName,
+  keyword,
+  generateResponse
+) {
   try {
     let prompt;
     const quotedMessage = await message.getQuotedMessage();
 
     if (quotedMessage) {
-      prompt = `${quotedMessage.body.trim()} ${message.body.replace(new RegExp(keyword, 'i'), '').trim()}`;
+      prompt = `${quotedMessage.body.trim()} ${message.body
+        .replace(new RegExp(keyword, "i"), "")
+        .trim()}`;
     } else {
-      prompt = message.body.replace(new RegExp(keyword, 'i'), '').trim();
+      prompt = message.body.replace(new RegExp(keyword, "i"), "").trim();
     }
 
     if (!prompt) {
@@ -29,13 +46,21 @@ async function handleGenerativeAI(message, senderName, keyword, generateResponse
       throw new Error(EMPTY_PROMPT_ERROR);
     }
 
-    console.log(`[${new Date().toLocaleString()}] Gerando resposta para o prompt: ${prompt}`);
+    console.log(
+      `[${new Date().toLocaleString()}] Gerando resposta para o prompt: ${prompt}`
+    );
     const response = await generateResponse(prompt);
 
-    console.log(`[${new Date().toLocaleString()}] Úsuario ${senderName} solicitou uma resposta para o prompt: ${prompt} e recebeu a resposta: ${response}`);
+    console.log(
+      `[${new Date().toLocaleString()}] Úsuario ${senderName} solicitou uma resposta para o prompt: ${prompt} e recebeu a resposta: ${response}`
+    );
     message.reply(response);
   } catch (error) {
-    console.error(`[${new Date().toLocaleString()}] Erro ao gerar resposta: ${error.message}`);
+    console.error(
+      `[${new Date().toLocaleString()}] Erro ao gerar resposta: ${
+        error.message
+      }`
+    );
   }
 }
 
@@ -93,8 +118,61 @@ async function handleCommand(client, message, command) {
       await printGroupList(client);
       break;
 
+    case "pokemon":
+      try {
+        const pokemon = await getRandomPokemonNameAndImage(senderName);
+        if (pokemon) {
+          const media = await MessageMedia.fromUrl(pokemon.imageUrl);
+          await client.sendMessage(message.from, media, {
+            caption: `Parabéns, você capturou: ${pokemon.name}`,
+          });
+        } else {
+          message.reply(
+            "Desculpe, ocorreu um erro ao buscar o Pokémon aleatório."
+          );
+        }
+      } catch (error) {
+        console.error("Erro ao obter Pokémon aleatório:", error);
+        message.reply(
+          "Desculpe, ocorreu um erro ao buscar o Pokémon aleatório."
+        );
+      }
+      break;
+
+    case "pokedex":
+      try {
+        const pokedex = await getUserPokemon(senderName); // Adicionado 'await' aqui
+        let pokemonList;
+
+        if (typeof pokedex === "string") {
+          // Se for uma string, vamos converter para array
+          pokemonList = pokedex.replace(/[\[\]']/g, "").split(", ");
+        } else if (Array.isArray(pokedex)) {
+          // Se já for um array, usamos diretamente
+          pokemonList = pokedex;
+        } else {
+          throw new Error("Formato de Pokédex inválido");
+        }
+
+        const sortedPokedex = pokemonList.sort();
+        const formattedPokedex = sortedPokedex
+          .map((pokemon, index) => `${index + 1}. ${pokemon}`)
+          .join("\n");
+        const responseMessage = `Sua Pokédex:\n${formattedPokedex}`;
+        await client.sendMessage(message.from, responseMessage);
+      } catch (error) {
+        console.error("Erro ao enviar Pokédex:", error);
+        await client.sendMessage(
+          message.from,
+          "Desculpe, ocorreu um erro ao buscar sua Pokédex."
+        );
+      }
+      break;
+
     default:
-      message.reply("Invalid command, try !menu to see the available commands.");
+      message.reply(
+        "Invalid command, try !menu to see the available commands."
+      );
   }
 }
 
