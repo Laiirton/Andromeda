247e07b254209027cbeeb2a56bd1ba60a8ccd44b
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-29 00:43:25 -0300

    add new commands

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -3,6 +3,8 @@ Você pode perguntar coisas ao coiso, tipo o chat gpt(Essa versão de modelo é 
 Obs para pessoas burras: Se você tentar pedir algo que seja considerado contra os direitos humanos ele não vai responder.
 
 *!fig*: 'Cria uma figurinha a partir de uma imagem ou vídeo de até 5 segundos.'
+*!pokemon*: 'Capture um pokemon aleatório e adicione ele no seu pokedex.'
+*!pokedex*: 'Veja seu pokedex com todos os pokemons que você já capturou.'
 *!transcribe*: 'Use esse comando em um audio para transcrever o audio em texto.'
 *!r34*: 'Para buscar imagens no site rule34.xxx. Exemplo: !r34 naruto'
 *!nsfw*: 'Para exibir os comandos NSFW.'
