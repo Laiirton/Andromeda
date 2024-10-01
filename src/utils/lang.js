0f944bcab624868119dad8929d7896cec0add8c9
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-30 23:01:04 -0300

    Update lang.js

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -5,6 +5,7 @@ Se você tentar pedir algo que seja considerado contra os direitos humanos ele n
 *!fig*: 'Cria uma figurinha a partir de uma imagem ou vídeo de até 5 segundos.'
 *!pokemon*: 'Capture um pokemon aleatório e adicione ele no seu pokedex.'
 *!pokedex*: 'Veja seu pokedex com todos os pokemons que você já capturou.'
+*!companion*: 'adicione um pokemon como seu companheiro e evolua ele.'
 *!transcribe*: 'Use esse comando em um audio para transcrever o audio em texto.'
 *!r34*: 'Busca uma imagem R34 aleatória ou com a tag especificada.'
 *!nsfw*: 'Para exibir os comandos NSFW.'
