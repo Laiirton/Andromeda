80cfd447e006eca414a4f07b760fc6ef3be22805
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-10-01 19:41:12 -0300

    feat: trade pokemon

--- src/services/pokemon/index.js
+++ src/services/pokemon/index.js
@@ -1,8 +1,13 @@
 import { getRandomPokemonNameAndImage, getUserPokemon } from './capture.js';
 import { chooseCompanion } from './companion.js';
+import { initiateTrade, respondToTrade, getPendingTradeForUser, getPendingTradesForUser } from './trade.js';
 
 export {
   getRandomPokemonNameAndImage,
   getUserPokemon,
-  chooseCompanion
+  chooseCompanion,
+  initiateTrade,
+  respondToTrade,
+  getPendingTradeForUser,
+  getPendingTradesForUser
 };
