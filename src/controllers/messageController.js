f8e78d23286e68ed2edc0507b73b68fa397b2dd3
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-10-07 21:06:46 -0300

    sistema para detectar os comandos caso seja digitado errado

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -33,6 +33,34 @@ const EMPTY_PROMPT_ERROR = "O prompt não pode estar vazio.";
 const PROMPT_REPLY = "Oi, você precisa me dizer o que deseja.";
 
 class MessageController {
+  static levenshteinDistance(a, b) {
+    const matrix = [];
+
+    for (let i = 0; i <= b.length; i++) {
+      matrix[i] = [i];
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+      matrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+      for (let j = 1; j <= a.length; j++) {
+        if (b.charAt(i - 1) === a.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1];
+        } else {
+          matrix[i][j] = Math.min(
+            matrix[i - 1][j - 1] + 1,
+            matrix[i][j - 1] + 1,
+            matrix[i - 1][j] + 1
+          );
+        }
+      }
+    }
+
+    return matrix[b.length][a.length];
+  }
+
   static async handleGenerativeAI(message, senderName, keyword, generateResponse) {
     try {
       const prompt = await this.extractPrompt(message, keyword);
@@ -108,7 +136,25 @@ class MessageController {
         await handler();
       }
     } else {
-      await message.reply("Comando inválido. Digite !menu para ver os comandos disponíveis.");
+      // Encontrar o comando mais próximo
+      const availableCommands = Object.keys(commandHandlers);
+      let closestCommand = '';
+      let minDistance = Infinity;
+
+      for (const cmd of availableCommands) {
+        const distance = this.levenshteinDistance(command, cmd);
+        if (distance < minDistance) {
+          minDistance = distance;
+          closestCommand = cmd;
+        }
+      }
+
+      // Sugerir o comando mais próximo se a distância for menor que 3
+      if (minDistance < 3) {
+        await message.reply(`Comando inválido. Você quis dizer "!${closestCommand}"? Digite !menu para ver todos os comandos disponíveis.`);
+      } else {
+        await message.reply("Comando inválido. Digite !menu para ver os comandos disponíveis.");
+      }
     }
   }
 
